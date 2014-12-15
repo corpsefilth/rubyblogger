57c0f492ff17e9d479f5d380689bade9ebc82d2e
corpsefilth <robertb@roberto-bahena.com>
2014-12-14 23:13:17 -0800

    first blogger commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+include ArticlesHelper
+
+	def index
+		@articles = Article.all
+	end
+	
+	def show
+		@article = Article.find(params[:id])
+		
+		@comment = Comment.new
+		@comment.article_id = @article.id
+	end
+	
+	def new
+		@article = Article.new
+	end
+	
+	def create
+		# fail 
+		#@article = Article.new
+		#@article.title = params[:article][:title]
+		#@article.body = params[:article][:body]
+		
+		@article = Article.new(article_params)
+		@article.save
+		
+		flash.notice = "New Article '#{@article.title}' created!"
+		
+		redirect_to article_path(@article)
+	end
+	
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		
+		flash.notice = "Article '#{@article.title}' succesfully deleted!"
+		
+		redirect_to articles_path
+	end
+	
+	def edit
+		@article = Article.find( params[:id] )
+	end
+	
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		#@article = Article.find(params[:id])
+		#@article.update_attributes( params[ :article ] )
+		
+		flash.notice = "Article '#{@article.title}' updated!"
+		
+		redirect_to article_path(@article)
+	end
+end
